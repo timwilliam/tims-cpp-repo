1164e4c5eda30d0fcb8a39b321658f6b523aea64
timwilliam <19308063+timwilliam@users.noreply.github.com>
2022-01-17 16:42:16 +0800

    Added skeleton code for more concepts

--- cpp-basic/src/classes.cpp
+++ cpp-basic/src/classes.cpp
@@ -9,20 +9,31 @@
 // - operator overloading
 // - static members, which describes the class in general
 
-class User{
-    std::string status = "Gold";
+// The idea of encapsulation is to hide the innerworking of a class to the public world
+// - user need only to worry about how to use the function, and not necessarily how the innerworking of the function works
+// - we can view it like an interface, the inner details is abstracted, to ensure user is using it in a proper way
+// - encapsulation is achieved with access modifiers, and through getters and setters
+// - getters and setters allows us to have access to private data members of a class
 
+class User{
     public:
-        std::string firstName, lastName;
+        std::string firstName, lastName, status;
     
-    // since status is private, it need a method to access it
-    std::string getStatus(){
-        return status;
-    }
+        void setStatus(std::string status){
+            if(status == "Gold" || status == "Silver" || status == "Bronze"){
+                this->status = status;
+            }
+            
+        }
+    
+        std::string getStatus(){
+            return status;
+        }
 };
 
 int main(){
     User user;
+    user.setStatus("Test");
     std::cout << user.getStatus() << std::endl;
     return 0;
-}
+};
